4ed48d03397e4ead9e80a0591f01efe418ac2052
Valentine Shemyako <v.shemyako@gmail.com>
2020-04-15 08:49:16 +0300

    Examined tilde expansion functionality

---
+++ commands/expansion/002_tilde_expansion.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Tilde is expanded to the home directory of a logged-in user
+echo 'My home directory is' ~
+
+# It's possible to output other users' home directories
+echo 'Home directory of a root user is' ~root
