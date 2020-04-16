2b233f5c020f99a2fb51610af9192e2285b95eb9
Valentine Shemyako <v.shemyako@gmail.com>
2020-04-16 23:18:04 +0300

    Corrected output of the last positional parameter

--- commands/parameters/004_output_last_argument.sh
+++ commands/parameters/004_output_last_argument.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-# Is not completed yet
-bash -c 'arguments_count="$#"; printf "$arguments_count"' -- '1' '2' 'Third'
+# If we want to expand all of the positional parameters into the list of the parameters we need to use '$@' syntax;
+# {} - curly braces if we want to expand a list then; -1 - to get the first elements from the end;
+bash -c 'arguments_count="$#"; printf "${@: -1}"' -- '1' '2' 'Third'
